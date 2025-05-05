5b999082a0800f972ca0bb6b54cbfe0f89fd3c3d
steja6872 <109953165+steja6872@users.noreply.github.com>
2025-05-05 11:56:01 -0400

    Update and rename FactorialTest.java to factorialtest.java

--- src/factorialtest.java
+++ src/factorialtest.java
@@ -3,7 +3,7 @@ package com.example;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class FactorialTest {
+class factorialtest {
 
     @Test
     public void testFactorialOfZero() {
